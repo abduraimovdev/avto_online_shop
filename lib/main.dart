23820273ee44fa59acb082ed0253c7908b45025e
abduraimovdev <kenzo778118@gmail.com>
2023-04-29 11:16:47 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:avto_online_shop/runner.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Runner());
+}
+
---
+++ lib/runner.dart
@@ -0,0 +1,18 @@
+import 'package:avto_online_shop/pages/welcome_page.dart';
+import 'package:flutter/material.dart';
+
+class Runner extends StatefulWidget {
+  const Runner({super.key});
+
+  @override
+  State<Runner> createState() => _RunnerState();
+}
+
+class _RunnerState extends State<Runner> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WelcomePage(),
+    );
+  }
+}
